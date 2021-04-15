7038ffff38878d7f87694296c0ee8a888e7b1406
fadhilsaheer <fadhilsaheer@gmail.com>
2021-04-15 14:38:50 +0530

    get all rooms fn

--- server/redis/helper.js
+++ server/redis/helper.js
@@ -1,6 +1,19 @@
 const client = require("./index");
 
 module.exports = {
+    getAllRooms: () => {
+        return new Promise((resolve, reject) => {
+            client.lrange("rooms", 0, -1, (err, reply)=>{
+                if(err){
+                    console.log('[ERROR] Cannot fetch rooms from redis')
+                    console.log(err.message);
+                }else{
+                    resolve(reply);
+                }
+            })
+        });
+    },
+
     createRoom: (roomData) => {
         return new Promise((resolve, reject) => {
             client.lpush("rooms", roomData, (err, reply) => {
