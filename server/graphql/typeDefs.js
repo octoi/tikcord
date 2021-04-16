c5232280a99ed757634e8bbb79ac720876034e94
fadhilsaheer <fadhilsaheer@gmail.com>
2021-04-16 14:41:42 +0530

    simplified type defs

--- server/graphql/typeDefs.js
+++ server/graphql/typeDefs.js
@@ -1,29 +1,13 @@
 const gql = require("graphql-tag");
 
 const typeDefs = gql`
-    type User{
-        id: ID!,
-        name: String!,
-        email: String!,
-        profile: String!,
-    },
-    type Room{
-        id: ID!,
-        name: String!,
-        host: User!,
-        description: String!,
-    },
     type Query{
-        getRooms: [Room],
-        getRoomUsers(room: ID!): [User!], 
+        getRooms: [String],
+        getRoom(room: ID!): String!, 
+        getRoomUsers(room: ID!): [String],
     },
-    input Host{
-        name: String!,
-        email: String!,
-        profile: String!,
-    }
     type Mutation{
-        createRoom(name: String!, description: String!, host: Host!): Room!,
+        createRoom(data: String!): Room!,
     }
 `
 
