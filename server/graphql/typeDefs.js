36b1a312b3827aec3474537458d9eb803a369a92
fadhilsaheer <fadhilsaheer@gmail.com>
2021-04-16 15:47:45 +0530

    typedefs

--- server/graphql/typeDefs.js
+++ server/graphql/typeDefs.js
@@ -1,8 +1,13 @@
 const gql = require("graphql-tag");
 
 const typeDefs = gql`
+    type Room {
+        id: ID!,
+        roomid: String!,
+        roomdata: String!,
+    },
     type Query{
-        getRooms: [String],
+        getRooms: [Room!],
         getRoom(room: ID!): String!, 
         getRoomUsers(room: ID!): [String],
     },
