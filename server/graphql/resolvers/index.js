f352067600f4b515886ec60ae874c2c2811375ca
fadhilsaheer <fadhilsaheer@gmail.com>
2021-04-17 22:14:51 +0530

    implemented queyr and mutation

--- server/graphql/resolvers/index.js
+++ server/graphql/resolvers/index.js
@@ -1,7 +1,13 @@
+const userResolvers = require("./user");
+const videoResolvers = require("./video");
+
 module.exports = {
     Query: {
-        
+        ...userResolvers.Query,
+        ...videoResolvers.Query  
     },
     Mutation: {
+        ...userResolvers.Mutation,
+        ...videoResolvers.Mutation
     }
 }
