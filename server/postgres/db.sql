57ac642e3d546889dbe2cab44e90f9bb0e80f245
fadhilsaheer <fadhilsaheer@gmail.com>
2021-04-17 22:33:35 +0530

    updated my sql stuff

--- server/postgres/db.sql
+++ server/postgres/db.sql
@@ -1,30 +1,30 @@
 CREATE DATABASE tikcord;
 
-CREATE TABLE tikuser{
+CREATE TABLE tikuser(
     id SERIAL PRIMARY KEY,
     name TEXT,
     email TEXT,
     password: TEXT,
     bio: TEXT,
     profile: TEXT,
-};
+);
 
-CREATE TABLE tikvideo{
+CREATE TABLE tikvideo(
     id SERIAL PRIMARY KEY,
     creator: BIGINT REFERENCES tikuser(id),
     content: TEXT,
     description: TEXT,
     createdAt: TEXT,
-}
+)
 
-CREATE TABLE tiklike{
+CREATE TABLE tiklike(
     id SERIAL PRIMARY KEY,
     creator: BIGINT REFERENCES tikuser(id),
-}
+)
 
-CREATE TABLE tikcomment{
+CREATE TABLE tikcomment(
     id SERIAL PRIMARY KEY,
     createdAt: TEXT,
     creator: BIGINT REFERENCES tikuser(id),
     content: TEXT,
-}
+)
