66c9552b9b9ea477b20a9070e28631b6bee3b85c
fadhilsaheer <fadhilsaheer@gmail.com>
2021-04-17 22:39:18 +0530

    fixed sql setup bug

--- server/postgres/createStuff.js
+++ server/postgres/createStuff.js
@@ -1,35 +1,35 @@
 module.exports = (pool) => {
-    pool.query('CREATE DATABASE tikcord');
     pool.query(`
-        CREATE TABLE tikuser{
+        CREATE TABLE IF NOT EXISTS tikuser(
             id SERIAL PRIMARY KEY,
             name TEXT,
             email TEXT,
-            password: TEXT,
-            bio: TEXT,
-        };
+            password TEXT,
+            bio TEXT
+        )
+    `);
+    pool.query("SELECT * FROM tikuser")
+    pool.query(`
+        CREATE TABLE IF NOT EXISTS tikvideo(
+            id SERIAL PRIMARY KEY,
+            creator BIGINT REFERENCES tikuser(id),
+            content TEXT,
+            description TEXT,
+            createdAt TEXT
+        )
     `);
     pool.query(`
-        CREATE TABLE tikvideo{
+        CREATE TABLE IF NOT EXISTS tiklike(
             id SERIAL PRIMARY KEY,
-            creator: BIGINT REFERENCES tikuser(id),
-            content: TEXT,
-            description: TEXT,
-            createdAt: TEXT,
-        }
-    `);
-    pool.query(`
-        CREATE TABLE tiklike{
-            id SERIAL PRIMARY KEY,
-            creator: BIGINT REFERENCES tikuser(id),
-        }
+            creator BIGINT REFERENCES tikuser(id)
+        )
     `)
     pool.query(`
-        CREATE TABLE tikcomment{
+        CREATE TABLE IF NOT EXISTS tikcomment(
             id SERIAL PRIMARY KEY,
-            createdAt: TEXT,
-            creator: BIGINT REFERENCES tikuser(id),
-            content: TEXT,
-        }
+            createdAt TEXT,
+            creator BIGINT REFERENCES tikuser(id),
+            content TEXT
+        )
     `)
 }
