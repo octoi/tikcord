128dab4e2b39ca493716ef4c1e275dd24d593a8a
fadhilsaheer <fadhilsaheer@gmail.com>
2021-04-17 18:11:28 +0530

    cleaned helper and setup

--- server/postgres/setup.js
+++ server/postgres/setup.js
@@ -5,7 +5,7 @@ const Pool = require("pg").Pool;
 const pool = new Pool({
     user: process.env.PG_USER,
     host: process.env.PG_HOST,
-    database: 'chatcord',
+    database: 'tikcord',
     password: process.env.PG_PASSWORD,
     port: process.env.PG_PORT
 })
