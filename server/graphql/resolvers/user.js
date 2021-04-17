65474bcfbeebf994ad15c8385e57bf30318b0a46
fadhilsaheer <fadhilsaheer@gmail.com>
2021-04-17 23:37:07 +0530

    login

--- server/graphql/resolvers/user.js
+++ server/graphql/resolvers/user.js
@@ -1,6 +1,8 @@
+const { AuthenticationError } = require("apollo-server-errors");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const { registerUser } = require("../../postgres/helper");
+const { checkUserExists } = require("../../postgres/utils/check");
 
 const generateToken = (user) => {
     return jwt.sign({
@@ -11,13 +13,28 @@ const generateToken = (user) => {
 }
 
 const Mutation = {
+
     register: async (_, { name, email, password, bio, profile }) => {
         password = await bcrypt.hash(password, 10);
         const registerData = await registerUser(name, email, password, bio, profile);
         
         const token = generateToken(registerData);
         return { ...registerData, token };
+    },
+
+    login: async (_, { email, password }) => {
+        const data = await checkUserExists(email);
+        const userData = data.rows[0]
+
+        if(!userData) return undefined;
+
+        const result = await bcrypt.compare(password, userData.password);
+        if(!result) throw new AuthenticationError("Authentication error");
+
+        const token = generateToken(userData);
+        return { ...userData, token }
     }
+
 }
 
 const Query = {}
