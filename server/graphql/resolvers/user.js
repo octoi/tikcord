8144f0295bb402654a263d5f91b49d0d9b808be7
fadhilsaheer <fadhilsaheer@gmail.com>
2021-04-17 22:53:20 +0530

    register user

--- server/graphql/resolvers/user.js
+++ server/graphql/resolvers/user.js
@@ -2,9 +2,9 @@ const bcrypt = require("bcrypt");
 const { registerUser } = require("../../postgres/helper");
 
 const Mutation = {
-    register: async (_, { name, email, password }) => {
+    register: async (_, { name, email, password, bio, profile }) => {
         password = await bcrypt.hash(password, 10);
-        const registerData = await registerUser(name, email, password);
+        const registerData = await registerUser(name, email, password, bio, profile);
         return registerData;
     }
 }
--- server/postgres/helpers/userHelper.js
+++ server/postgres/helpers/userHelper.js
@@ -2,10 +2,10 @@ const pool = require("../setup");
 const { userTable } = require("../constants");
 
 module.exports = {
-    registerUser: (name, email, password) => {
+    registerUser: (name, email, password, bio, profile) => {
         return new Promise((resolve, reject) => {
-            pool.query(`INSERT INTO ${userTable} (name, email, password) VALUES ($1, $2, $3)`, [name, email, password]).then(data => {
-                resolve(data.fields);
+            pool.query(`INSERT INTO ${userTable} (name, email, password, bio, profile) VALUES ($1, $2, $3, $4, $5)`, [name, email, password, bio, profile]).then(data => {
+                resolve({ name, email, password, bio, profile });
             }).catch(err => {
                 console.log(err.message)
                 process.exit(0)
