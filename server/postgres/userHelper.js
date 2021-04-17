c1581ac5c91584ccdf20b9afc4686ccb04c10475
fadhilsaheer <fadhilsaheer@gmail.com>
2021-04-17 18:26:52 +0530

    register user

---
+++ server/postgres/userHelper.js
@@ -0,0 +1,15 @@
+const pool = require("./setup");
+const { userTable } = require("./constants");
+
+module.exports = {
+    registerUser: (name, email, password) => {
+        return new Promise((resolve, reject) => {
+            pool.query(`INSERT INTO ${userTable} (name, email, password) VALUES ($1, $2, $3)`, [name, email, password]).then(data => {
+                resolve(data.fields);
+            }).catch(err => {
+                console.log(err.message)
+                process.exit(0)
+            })
+        });
+    }
+}
