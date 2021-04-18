8d45c61e82af031a7635db4751298c5b6d452e9a
fadhilsaheer <fadhilsaheer@gmail.com>
2021-04-18 07:45:02 +0530

    created video in db function

---
+++ server/postgres/helpers/videoHelper.js
@@ -0,0 +1,17 @@
+const pool = require("../setup");
+const { videoTable } = require("../constants");
+
+module.exports = {
+    createVideo: (videoData) => {
+        return new Promise((resolve, reject) => {
+            const { creator, content, description, createdAt, likeCount, commentCount } = videoData;
+
+            pool.query(`INSERT INTO ${videoTable} (creator, content, description, createdAt, likeCount, commentCount) VALUES ($1, $2, $3, $4, $5, $6)`, [creator, content, description, createdAt, likeCount, commentCount]).then(() => {
+                resolve(videoData)
+            }).catch(err => {
+                console.log(err.message)
+                reject();
+            })
+        });
+    }
+}
