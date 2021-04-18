4f5c59c561b40e21b129f4590d15b9bba71bb47b
fadhilsaheer <fadhilsaheer@gmail.com>
2021-04-18 08:06:48 +0530

    create video

--- server/graphql/resolvers/video.js
+++ server/graphql/resolvers/video.js
@@ -1,5 +1,28 @@
+const checkAuth = require("../utils/checkAuth");
+const { createVideo } = require("../../postgres/helper");
 
-const Mutation = {}
+const Mutation = {
+
+    createVideo: async (_, videoData, context) => {
+        const user = checkAuth(context);
+
+        videoData = {
+            ...videoData,
+            creator: user.id,
+            createdAt: Date.now(),
+            likeCount: 0,
+            commentCount: 0,
+        }
+
+        const data = await createVideo(videoData);
+        return {
+            ...data,
+            creator: user
+        }
+
+    }
+
+}
 
 const Query = {}
 
