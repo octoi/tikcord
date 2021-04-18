bf37a753e84567476dafeef145036af60913916a
fadhilsaheer <fadhilsaheer@gmail.com>
2021-04-18 08:16:19 +0530

    returned id of video

--- server/postgres/helpers/videoHelper.js
+++ server/postgres/helpers/videoHelper.js
@@ -6,8 +6,10 @@ module.exports = {
         return new Promise((resolve, reject) => {
             const { creator, content, description, createdAt, likeCount, commentCount } = videoData;
 
-            pool.query(`INSERT INTO ${videoTable} (creator, content, description, createdAt, likeCount, commentCount) VALUES ($1, $2, $3, $4, $5, $6)`, [creator, content, description, createdAt, likeCount, commentCount]).then((data) => {
-                resolve(videoData)
+            pool.query(`INSERT INTO ${videoTable} (creator, content, description, createdAt, likeCount, commentCount) VALUES ($1, $2, $3, $4, $5, $6)`, [creator, content, description, createdAt, likeCount, commentCount]).then(() => {
+                pool.query(`SELECT * FROM ${videoTable} WHERE createdAt = $1`, [createdAt]).then(({ rows }) => {
+                    resolve({ ...videoData, id: rows[0].id });
+                })
             }).catch(err => {
                 console.log(err.message)
                 reject();
