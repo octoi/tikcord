ddff208553445d10ec1e1001d0ded0801da5b006
fadhilsaheer <fadhilsaheer@gmail.com>
2021-04-18 07:28:35 +0530

    passed req as context

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const { ApolloServer } = require("apollo-server");
 const typeDefs = require("./graphql/typeDefs");
 const resolvers = require("./graphql/resolvers");
 
-const server = new ApolloServer({ typeDefs, resolvers}); // graphql server
+const server = new ApolloServer({ typeDefs, resolvers, context: ({ req }) => ({ req }) }); // graphql server
 
 const port = process.env.PORT || 5000;
 server.listen(port).then(({ url })=> console.log(`[INFO] Server started at ${url}`))
