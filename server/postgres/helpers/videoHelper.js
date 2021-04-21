bb1f8bc269b9e6e7909c17ba30ee3aaf7b827ed9
fadhilsaheer <fadhilsaheer@gmail.com>
2021-04-21 12:29:31 +0530

    checking and liking

--- server/postgres/helpers/videoHelper.js
+++ server/postgres/helpers/videoHelper.js
@@ -81,7 +81,22 @@ module.exports = {
 
     likeAVideo: ({ creator, video }) => {
         return new Promise((resolve, reject) => {
-            // like video
+
+            pool.query(`SELECT * FROM ${likeTable} WHERE video = $1 AND creator = $2`, [video, creator]).then(({ rows: data }) => {
+                if(data.length > 0){
+                    pool.query(`INSERT INTO ${likeTable} (creator, video) VALUES ($1 $2)`, [creator, video]).then(({ rows: data }) => {
+                        resolve(data);
+                    })                    
+                }else{
+                    pool.query(`DELETE FROM ${likeTable} WHERE video = $1 AND creator = $2`, [video, creator]).then(({ rows: data }) => {
+                        resolve(data)
+                    });
+                }
+            }).catch(err => {
+                console.log(err.message);
+                reject();
+            });
+
         });
     },
 
