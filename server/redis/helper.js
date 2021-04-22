8707817dc9fbc4a54113b4362a342e0ac4483cb9
fadhilsaheer <fadhilsaheer@gmail.com>
2021-04-22 07:17:44 +0530

    cleaned helper and added getting from cache

--- server/redis/helper.js
+++ server/redis/helper.js
@@ -1,29 +1,16 @@
 const client = require("./index");
 
 module.exports = {
-    getRoomsFromDb: () => {
+    getVideosFromCache: () => {
         return new Promise((resolve, reject) => {
-            client.lrange("rooms", 0, -1, (err, reply)=>{
+            client.lrange("videos", 0, -1, (err, reply) => {
                 if(err){
-                    console.log('[ERROR] Cannot fetch rooms from redis')
                     console.log(err.message);
+                    reject();
                 }else{
-                    resolve(reply);
+                    resolve(reply)
                 }
-            })
-        });
-    },
-
-    createRoomInDb: (roomData) => {
-        return new Promise((resolve, reject) => {
-            client.lpush("rooms", JSON.stringify(roomData), (err, reply) => {
-                if(err){
-                    console.log('[ERROR] Cannot add room data to redis')
-                    console.log(err.message);
-                }else{
-                    resolve(reply);
-                }
-            })
+            });
         });
     }
 }
