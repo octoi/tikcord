2ae23daf29b612fbf4f1b4ed24f06384970a151b
fadhilsaheer <fadhilsaheer@gmail.com>
2021-04-23 22:19:40 +0530

    login sample

---
+++ client/pages/account/login.js
@@ -0,0 +1,12 @@
+import Head from 'next/head';
+
+export default function Login() {
+    return (
+        <section>
+            <Head>
+                <title>Login</title>
+                <meta name="description" content="Login to unleash your creativity" />
+            </Head>
+        </section>
+    );
+}
