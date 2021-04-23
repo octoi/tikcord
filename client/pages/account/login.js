d50797b3b5a467fe2fe78cfe0e3209eff7d007fb
fadhilsaheer <fadhilsaheer@gmail.com>
2021-04-23 22:55:06 +0530

    utils

--- client/pages/account/login.js
+++ client/pages/account/login.js
@@ -2,7 +2,7 @@ import Head from 'next/head';
 import useAuthContext from '../../context/contextHook';
 import styles from '../../styles/Login.module.css';
 import { useRouter } from 'next/router';
-import { Text, Input, Button } from '@chakra-ui/react';
+import { Text, Input, Button, Link } from '@chakra-ui/react';
 
 export default function Login() {
     const { user, setUser } = useAuthContext();
@@ -24,7 +24,10 @@ export default function Login() {
                 <form style={{ marginTop: "50px" }}>
                     <Input placeholder="Email address" />
                     <Input className={styles.mt} placeholder="Password" type="password" />
-                    <Button className={styles.mt} colorScheme="teal" variant="outline">Login</Button>
+                    <div className={styles.utils}>
+                        <Button colorScheme="teal" variant="outline">Login</Button>
+                        <Link href="/account/register" color="teal.200">Don't have an account ?? register</Link>
+                    </div>
                 </form>
             </div>
         </section>
