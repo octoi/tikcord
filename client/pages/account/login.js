3f10341d61b435e74421027116a51280dc91fe3e
fadhilsaheer <fadhilsaheer@gmail.com>
2021-04-23 22:35:41 +0530

    basic setup

--- client/pages/account/login.js
+++ client/pages/account/login.js
@@ -1,6 +1,14 @@
 import Head from 'next/head';
+import useAuthContext from '../../context/contextHook';
+import { useRouter } from 'next/router';
+// import {  } from '@chakra-ui/react';
 
 export default function Login() {
+    const { user, setUser } = useAuthContext();
+    const router = useRouter();
+
+    if (user) router.push('/app');
+
     return (
         <section>
             <Head>
