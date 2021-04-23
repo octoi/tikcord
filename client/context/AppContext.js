5743ddefc08333a09c5c5982e8679fb2c4ab93e6
fadhilsaheer <fadhilsaheer@gmail.com>
2021-04-23 08:26:30 +0530

    context

---
+++ client/context/AppContext.js
@@ -0,0 +1,18 @@
+import { createContext, useState } from 'react';
+import { getUser } from './auth';
+
+export const UserStateContext = createContext();
+
+export function AppContext({ children }) {
+    const [user, setUser] = useState({});
+
+    const userFromSession = getUser();
+    if (userFromSession) setUser(userFromSession);
+
+    return (
+        <UserStateContext.Provider value={{ user, setUser }}>
+            {children}
+        </UserStateContext.Provider>
+    );
+
+}
