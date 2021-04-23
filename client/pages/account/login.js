8d5c5e7df1ab2702642d31d6fabd0e66e54e5fd0
fadhilsaheer <fadhilsaheer@gmail.com>
2021-04-23 22:49:28 +0530

    login form

--- client/pages/account/login.js
+++ client/pages/account/login.js
@@ -1,13 +1,16 @@
 import Head from 'next/head';
 import useAuthContext from '../../context/contextHook';
+import styles from '../../styles/Login.module.css';
 import { useRouter } from 'next/router';
-// import {  } from '@chakra-ui/react';
+import { Text, Input, Button } from '@chakra-ui/react';
 
 export default function Login() {
     const { user, setUser } = useAuthContext();
     const router = useRouter();
 
-    if (user) router.push('/app');
+    if (user.name) {
+        router.push('/app');
+    }
 
     return (
         <section>
@@ -15,6 +18,15 @@ export default function Login() {
                 <title>Login</title>
                 <meta name="description" content="Login to unleash your creativity" />
             </Head>
+
+            <div className={styles.container}>
+                <Text fontSize="4xl" style={{ fontWeight: "600" }}>Login Tikcord</Text>
+                <form style={{ marginTop: "50px" }}>
+                    <Input placeholder="Email address" />
+                    <Input className={styles.mt} placeholder="Password" type="password" />
+                    <Button className={styles.mt} colorScheme="teal" variant="outline">Login</Button>
+                </form>
+            </div>
         </section>
     );
 }
