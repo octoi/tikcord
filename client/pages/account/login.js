db1b1086bb932c8b2ddffa463df62c062a5a7855
fadhilsaheer <fadhilsaheer@gmail.com>
2021-04-24 11:46:37 +0530

    state form

--- client/pages/account/login.js
+++ client/pages/account/login.js
@@ -2,10 +2,12 @@ import Head from 'next/head';
 import useAuthContext from '../../context/contextHook';
 import styles from '../../styles/Login.module.css';
 import { useRouter } from 'next/router';
+import { useState } from 'react';
 import { Text, Input, Button, Link } from '@chakra-ui/react';
 
 export default function Login() {
     const { user, setUser } = useAuthContext();
+    const [loginUser, setLoginUser] = useState({ email: '', password: '' });
     const router = useRouter();
 
     if (user.name) {
@@ -13,7 +15,7 @@ export default function Login() {
     }
 
     const submitForm = (event) => {
-        event.preventDefault()
+        event.preventDefault();
     }
 
     return (
@@ -26,8 +28,21 @@ export default function Login() {
             <div className={styles.container}>
                 <Text fontSize="4xl" style={{ fontWeight: "600" }}>Login Tikcord</Text>
                 <form style={{ marginTop: "50px" }} onSubmit={submitForm}>
-                    <Input placeholder="Email address" type="email" />
-                    <Input className={styles.mt} placeholder="Password" type="password" />
+
+                    <Input
+                        placeholder="Email address"
+                        type="email"
+                        value={loginUser.email}
+                        onChange={e => setLoginUser({ ...loginUser, email: e.target.value })}
+                    />
+                    <Input
+                        className={styles.mt}
+                        placeholder="Password"
+                        type="password"
+                        value={loginUser.password}
+                        onChange={e => setLoginUser({ ...loginUser, password: e.target.value })}
+                    />
+
                     <div className={styles.utils}>
                         <Button colorScheme="teal" variant="outline">Login</Button>
                         <Link href="/account/register" color="teal.200">Don't have an account ?? register</Link>
