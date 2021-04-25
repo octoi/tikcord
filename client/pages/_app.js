9b36c7c2a894005c2ba22bb90954a894b03a5882
fadhilsaheer <fadhilsaheer@gmail.com>
2021-04-25 10:44:28 +0530

    setuped apollo provider

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,7 +1,8 @@
 import '../styles/globals.css';
+import Header from '../components/shared/Header';
 import { ChakraProvider, extendTheme } from '@chakra-ui/react';
 import { AppContext } from '../context/AppContext';
-import Header from '../components/shared/Header';
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 
 const theme = extendTheme({
     config: {
@@ -10,13 +11,20 @@ const theme = extendTheme({
     }
 });
 
+const client = new ApolloClient({
+    uri: process.env.SERVER_URL || 'http://localhost:5000',
+    cache: new InMemoryCache(),
+});
+
 function MyApp({ Component, pageProps }) {
     return (
         <ChakraProvider theme={theme}>
-            <AppContext>
-                <Header />
-                <Component {...pageProps} />
-            </AppContext>
+            <ApolloProvider client={client}>
+                <AppContext>
+                    <Header />
+                    <Component {...pageProps} />
+                </AppContext>
+            </ApolloProvider>
         </ChakraProvider>
     )
 }
