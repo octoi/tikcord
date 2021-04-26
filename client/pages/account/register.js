264661be93c084860cfb980f17b921fadcb03138
fadhilsaheer <fadhilsaheer@gmail.com>
2021-04-26 21:29:16 +0530

    reguster form

--- client/pages/account/register.js
+++ client/pages/account/register.js
@@ -6,6 +6,8 @@ import { Text, Input, Button, Link } from '@chakra-ui/react';
 
 export default function Register() {
 
+    const [loginUser, setLoginUser] = useState({ name: '', email: '', password: '', repass: '' });
+
     const submitForm = (event) => {
         event.preventDefault();
     }
@@ -17,10 +19,18 @@ export default function Register() {
                 <meta name="description" content="register to unleash your creativity" />
             </Head>
 
-            <div className="styles.container">
+            <div className={styles.container}>
+                <Text fontSize="4xl" style={{ fontWeight: "600" }}>Register Tikcord</Text>
                 <form style={{ marginTop: "50px" }} onSubmit={submitForm}>
 
                     <Input
+                        placeholder="Full name"
+                        type="text"
+                        onChange={e => setLoginUser({ ...loginUser, name: e.target.value })}
+                        required
+                    />
+                    <Input
+                        className={styles.mt}
                         placeholder="Email address"
                         type="email"
                         onChange={e => setLoginUser({ ...loginUser, email: e.target.value })}
@@ -33,10 +43,17 @@ export default function Register() {
                         onChange={e => setLoginUser({ ...loginUser, password: e.target.value })}
                         required
                     />
+                    <Input
+                        className={styles.mt}
+                        placeholder="Retype password"
+                        type="password"
+                        onChange={e => setLoginUser({ ...loginUser, repass: e.target.value })}
+                        required
+                    />
 
                     <div className={styles.utils}>
-                        <Button colorScheme="teal" variant="outline">Login</Button>
-                        <Link href="/account/register" color="teal.200">Don't have an account ?? register</Link>
+                        <Button colorScheme="teal" variant="outline">Register</Button>
+                        <Link href="/account/login" color="teal.200">Already have an account ?? Login</Link>
                     </div>
                 </form>
             </div>
