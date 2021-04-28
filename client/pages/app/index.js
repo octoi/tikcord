8b44e492ace8d5041c2127df7cfea1bdfebc3c88
fadhilsaheer <fadhilsaheer@gmail.com>
2021-04-28 14:06:13 +0530

    created and user components

--- client/pages/app/index.js
+++ client/pages/app/index.js
@@ -2,6 +2,13 @@ import Head from 'next/head';
 import useAuthContext from '../../context/contextHook';
 import { useEffect } from 'react';
 import { useRouter } from 'next/router';
+import { Grid, GridItem } from '@chakra-ui/react';
+
+// components
+
+import Sidebar from '../../components/app/Sidebar';
+import Feed from '../../components/app/Feed';
+import Users from '../../components/app/Users';
 
 export default function App() {
     const { user, setUser } = useAuthContext();
@@ -17,6 +24,17 @@ export default function App() {
                 <title>Tikcord</title>
                 <meta name="description" content="Unleash your creativity" />
             </Head>
+
+            <Grid
+                gap={4}
+                templateColumns="repeat(3, 1fr)"
+                className={ }
+            >
+                <GridItem><Sidebar /></GridItem>
+                <GridItem><Feed /></GridItem>
+                <GridItem><Users /></GridItem>
+            </Grid>
+
         </section>
     );
 
