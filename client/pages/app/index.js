b7171acd2f039d2d873380965d5fa2f6ec8bd3ce
fadhilsaheer <fadhilsaheer@gmail.com>
2021-04-28 13:46:40 +0530

    sample app page

---
+++ client/pages/app/index.js
@@ -0,0 +1,14 @@
+import Head from 'next/head';
+
+export default function App() {
+
+    return (
+        <section>
+            <Head>
+                <title>Tikcord</title>
+                <meta name="description" content="Unleash your creativity" />
+            </Head>
+        </section>
+    );
+
+}
