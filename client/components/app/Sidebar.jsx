929129dedc8090eeebb4a2edf5e7115cb6556560
fadhilsaheer <fadhilsaheer@gmail.com>
2021-04-28 14:28:23 +0530

    created components

--- client/components/app/Sidebar.jsx
+++ client/components/app/Sidebar.jsx
@@ -1,7 +1,13 @@
+import IconButton from './sidebar/IconButtton';
+import { HomeIcon, UserIcon, CogIcon, PlusCircleIcon } from '@heroicons/react/solid'
+
 export default function Sidebar() {
     return (
         <div>
-            <h2>sidebar</h2>
+            <IconButton Icon={HomeIcon} text="Home" link="/app" />
+            <IconButton Icon={UserIcon} text="Profile" link="/profile" mt />
+            <IconButton Icon={CogIcon} text="Settings" link="/settings" mt />
+            <IconButton Icon={PlusCircleIcon} text="Create" link="/new" mt solid />
         </div>
     );
 }
