d78ce71e0d9b89f69db2c441ded5dbb3b36a8357
fadhilsaheer <fadhilsaheer@gmail.com>
2021-04-28 14:32:12 +0530

    redirect to specific page

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,6 +1,22 @@
 import Head from 'next/head';
+import useAuthContext from '../context/contextHook';
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
 
 export default function Home() {
+	const { user } = useAuthContext();
+	const router = useRouter();
+
+	useEffect(() => {
+		if (Object.keys(user).length > 0) {
+			router.push('/app')
+			return
+		}
+
+		router.push('/account/login')
+
+	}, [user, router])
+
 	return (
 		<div>
 			<Head>
