315a4c4619514e102cd53e230560bdc379fb5cea
fadhilsaheer <fadhilsaheer@gmail.com>
2021-04-28 13:50:23 +0530

    added check for user

--- client/pages/app/index.js
+++ client/pages/app/index.js
@@ -1,6 +1,15 @@
 import Head from 'next/head';
+import useAuthContext from '../../context/contextHook';
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
 
 export default function App() {
+    const { user, setUser } = useAuthContext();
+    const router = useRouter();
+
+    useEffect(() => {
+        if (Object.keys(user).length > 0) router.push('/app');
+    }, [user, router]);
 
     return (
         <section>
