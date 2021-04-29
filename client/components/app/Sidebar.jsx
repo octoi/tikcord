cc6f886ec42aba296dd57d85f324da725c78c491
fadhilsaheer <fadhilsaheer@gmail.com>
2021-04-29 22:18:19 +0530

    deleted sample btn

--- client/components/app/Sidebar.jsx
+++ client/components/app/Sidebar.jsx
@@ -1,13 +1,18 @@
 import IconButton from './sidebar/IconButtton';
+import NewPost from './NewPost';
 import { HomeIcon, UserIcon, CogIcon, PlusCircleIcon } from '@heroicons/react/solid'
+import { Button, useDisclosure } from '@chakra-ui/react';
 
 export default function Sidebar() {
+    const { isOpen, onOpen, onClose } = useDisclosure();
+
     return (
         <div>
             <IconButton Icon={HomeIcon} text="Home" link="/app" />
             <IconButton Icon={UserIcon} text="Profile" link="/app/profile" mt />
             <IconButton Icon={CogIcon} text="Settings" link="/app/settings" mt />
-            <IconButton Icon={PlusCircleIcon} text="Create" link="/app" mt solid />
+            <IconButton Icon={PlusCircleIcon} text="Create" mt solid onClick={onOpen} />
+            <NewPost isOpen={isOpen} onClose={onClose} />
         </div>
     );
 }
