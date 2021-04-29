0329218a94cd65916241e4b6d24512d21e54c080
fadhilsaheer <fadhilsaheer@gmail.com>
2021-04-29 06:19:28 +0530

    added some btn style

--- client/components/app/sidebar/IconButtton.jsx
+++ client/components/app/sidebar/IconButtton.jsx
@@ -9,7 +9,7 @@ export default function IconButton({ Icon, text, link, mt, solid }) {
             colorScheme="twitter"
             variant={solid ? "solid" : "ghost"}
             onClick={() => router.push(link)}
-            style={{ display: "flex", alignItems: "center", marginTop: mt ? 20 : 0 }}
+            style={{ display: "flex", alignItems: "center", marginTop: mt ? 20 : 0, width: 150, justifyContent: "start" }}
         >
             <Icon style={{ width: 20, height: 20, marginRight: 10 }} />
             <p style={{ fontSize: 18 }}>{text}</p>
