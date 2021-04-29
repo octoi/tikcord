01f81b58e1938b7469fdbf5c9184f9728edcb966
fadhilsaheer <fadhilsaheer@gmail.com>
2021-04-29 06:47:31 +0530

    onclick

--- client/components/app/feed/NoPosts.jsx
+++ client/components/app/feed/NoPosts.jsx
@@ -1,11 +1,14 @@
 import { Text, Button } from '@chakra-ui/react';
 import { EmojiSadIcon } from '@heroicons/react/outline';
+import { useRouter } from 'next/router';
 
 export default function NoPosts() {
+    const router = useRouter();
+
     return (
         <section style={{ marginTop: 40 }}>
             <Text fontSize="xl" style={{ display: "flex" }} >No tiks yet <EmojiSadIcon style={{ width: 30, marginLeft: 5 }} /></Text>
-            <Button onClick={ } style={{ marginTop: 20 }}>Create One</Button>
+            <Button onClick={() => router.push("/app/new")} style={{ marginTop: 20 }}>Create One</Button>
         </section>
     );
 }
