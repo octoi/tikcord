fa437fdc3c6ba89346020dc241ad674cd97ecbc5
fadhilsaheer <fadhilsaheer@gmail.com>
2021-04-29 06:29:16 +0530

    added text

--- client/components/app/Feed.jsx
+++ client/components/app/Feed.jsx
@@ -1,7 +1,11 @@
+import styles from '../../styles/App.module.css';
+import { Text } from '@chakra-ui/react';
+
 export default function Sidebar() {
     return (
-        <div>
-            <h2>sidebar</h2>
+        <div className={styles.feedContainer}>
+            <Text fontSize="2xl">Your Feed</Text>
+
         </div>
     );
 }
