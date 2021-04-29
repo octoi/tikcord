900246d84e5a7fcb3f3604c7bfe378012b2da139
fadhilsaheer <fadhilsaheer@gmail.com>
2021-04-29 21:52:01 +0530

    removed sample video

--- client/components/app/Feed.jsx
+++ client/components/app/Feed.jsx
@@ -1,6 +1,6 @@
 import styles from '../../styles/App.module.css';
 import NoPosts from './feed/NoPosts';
-import { Text, AspectRatio } from '@chakra-ui/react'
+import { Text } from '@chakra-ui/react'
 
 export default function Sidebar({ posts }) {
     return (
@@ -8,15 +8,6 @@ export default function Sidebar({ posts }) {
             <div className={styles.feedSubContainer}>
                 <Text fontSize="2xl">Your Feed</Text>
                 {!posts && <NoPosts />}
-                <AspectRatio style={{ width: 500 }} ratio={16 / 9}>
-                    <iframe
-                        src="https://www.youtube.com/embed/dQw4w9WgXcQ?controls=0"
-                        title="YouTube video player"
-                        frameborder="0"
-                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                        allowfullscreen
-                    />
-                </AspectRatio>
             </div>
         </section>
     );
