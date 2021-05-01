e233256aa513de8e0c0f020d5b1db3b311f490f8
fadhilsaheer <fadhilsaheer@gmail.com>
2021-05-01 11:51:49 +0530

    sample state

--- client/components/app/feed/AllPosts.jsx
+++ client/components/app/feed/AllPosts.jsx
@@ -1,7 +1,13 @@
+import { useState } from 'react';
+
 export default function AllPosts() {
+    const [posts, setPosts] = useState([]);
+
     return (
         <div>
-
+            {posts.length > 0 && posts.map(post => (
+                <p>post</p>
+            ))}
         </div>
     );
 }
