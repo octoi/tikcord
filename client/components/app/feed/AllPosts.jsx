fe2848f017f1fa12e2fc8448f6fe688cc880b0bb
fadhilsaheer <fadhilsaheer@gmail.com>
2021-05-01 21:53:08 +0530

    cleanup

--- client/components/app/feed/AllPosts.jsx
+++ client/components/app/feed/AllPosts.jsx
@@ -1,17 +1,8 @@
-import FETCH_POSTS_QUERY from '../../../utils/graphql/fetchPostsQuery';
-import { useState } from 'react';
-import { useQuery } from '@apollo/client';
-
-export default function AllPosts() {
-    const [posts, setPosts] = useState([]);
-
-    const { loading, data } = useQuery(FETCH_POSTS_QUERY);
-    console.log(data)
-
+export default function AllPosts({ posts, loading }) {
     return (
         <div>
             {posts && posts.map(post => (
-                <p>post</p>
+                <p key={post.id}>post</p>
             ))}
         </div>
     );
