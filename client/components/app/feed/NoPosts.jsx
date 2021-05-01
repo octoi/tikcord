ca55bfb02d4430bff86871e24ec68a418abd0e3e
fadhilsaheer <fadhilsaheer@gmail.com>
2021-05-01 07:54:20 +0530

    removed create button

--- client/components/app/feed/NoPosts.jsx
+++ client/components/app/feed/NoPosts.jsx
@@ -8,7 +8,6 @@ export default function NoPosts() {
     return (
         <section style={{ marginTop: 40 }}>
             <Text fontSize="xl" style={{ display: "flex" }} >No tiks yet <EmojiSadIcon style={{ width: 30, marginLeft: 5 }} /></Text>
-            <Button onClick={() => router.push("/app/new")} style={{ marginTop: 20 }}>Create One</Button>
         </section>
     );
 }
