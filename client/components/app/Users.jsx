75d1ab502bf5640adc69ea885f5fcc10c46a8f85
fadhilsaheer <fadhilsaheer@gmail.com>
2021-05-02 07:40:01 +0530

    users section

--- client/components/app/Users.jsx
+++ client/components/app/Users.jsx
@@ -1,7 +1,40 @@
-export default function Sidebar() {
+import User from './users/User';
+import { useState, useEffect } from 'react';
+import { Text } from '@chakra-ui/react';
+
+function getUsers(posts) {
+    const users = [];
+    const tracked = {};
+
+    if (!posts) return users;
+
+    posts.forEach(post => {
+        let user = post.creator;
+
+        if (tracked[user.email]) return;
+
+        users.push(user);
+        tracked[user.email] = user.email;
+
+    });
+
+    return users;
+}
+
+export default function Users({ posts }) {
+    const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+        const newUsers = getUsers(posts);
+        setUsers(newUsers);
+    }, [posts])
+
     return (
-        <div>
-            <h2>sidebar</h2>
-        </div>
+        <section style={{ margin: "10px 100px" }}>
+            <Text fontSize="2xl">Users</Text>
+            {users && users.map((user, idx) => (
+                <User key={idx} user={user} />
+            ))}
+        </section>
     );
 }
