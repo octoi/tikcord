22106b9895419e5ddc0952c1c2cf7f57fff9702a
fadhilsaheer <fadhilsaheer@gmail.com>
2021-05-02 07:23:25 +0530

    displayed btn

--- client/components/app/feed/Post.jsx
+++ client/components/app/feed/Post.jsx
@@ -1,17 +1,27 @@
 import styles from '../../../styles/Post.module.css';
-import { Image, Avatar, Text } from '@chakra-ui/react';
+import useAuthContext from '../../../context/contextHook';
+import { Image, Avatar, Text, Button } from '@chakra-ui/react';
 
 export default function Post({ post }) {
-    console.log(post);
+    const { user } = useAuthContext();
+
     return (
         <div className={styles.container}>
-            <div className={styles.profile}>
-                <Avatar width={70} height={70} src={post.creator.profile} />
-                <div style={{ marginLeft: "10px" }}>
-                    <Text fontSize="xl">{post.creator.name}</Text>
-                    <Text style={{ color: "#CBD5E0" }} fontSize="lg">{post.creator.email}</Text>
+            <div className={styles.topper}>
+                <div className={styles.profile}>
+                    <Avatar width={70} height={70} src={post.creator.profile} />
+                    <div style={{ marginLeft: "10px" }}>
+                        <Text fontSize="xl">{post.creator.name}</Text>
+                        <Text style={{ color: "#CBD5E0" }} fontSize="lg">{post.creator.email}</Text>
+                    </div>
                 </div>
+                {user?.email == post.creator.email ? (
+                    <Button variant="outline" colorScheme="twitter">DELETE</Button>
+                ) : (
+                    <Button variant="outline" colorScheme="twitter">VIEW</Button>
+                )}
             </div>
+
             <Image className={styles.image} src={post.content} alt={post.description} />
             <p>{post.description}</p>
         </div>
