804193914faf933087de3fa4b18a2a465e19526e
fadhilsaheer <fadhilsaheer@gmail.com>
2021-05-02 08:07:06 +0530

    removed display of my own profile

--- client/components/app/users/User.jsx
+++ client/components/app/users/User.jsx
@@ -1,13 +1,21 @@
+import useAuthContext from '../../../context/contextHook';
 import { Text, Avatar } from '@chakra-ui/react';
 
 export default function User({ user }) {
+    const { user: currentUser } = useAuthContext();
+    const displayUser = currentUser?.email != user.email;
+
     return (
-        <div style={{ display: "flex", marginTop: "10px", alignItems: "center" }}>
-            <Avatar src={user.profile} name={user.name} />
-            <div style={{ marginLeft: "10px" }}>
-                <Text fontSize="lg">{user.name}</Text>
-                <Text style={{ color: "#CBD5E0" }} fontSize="md">{user.email}</Text>
-            </div>
-        </div>
+        <>
+            {displayUser && (
+                <div style={{ display: "flex", marginTop: "10px", alignItems: "center" }}>
+                    <Avatar src={user.profile} name={user.name} />
+                    <div style={{ marginLeft: "10px" }}>
+                        <Text fontSize="lg">{user.name}</Text>
+                        <Text style={{ color: "#CBD5E0" }} fontSize="md">{user.email}</Text>
+                    </div>
+                </div>
+            )}
+        </>
     );
 }
