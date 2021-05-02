0e2dd01f39ebb82a31e72343913f148b17dc7b95
fadhilsaheer <fadhilsaheer@gmail.com>
2021-05-02 08:02:04 +0530

    added name for avatar

--- client/components/app/feed/Post.jsx
+++ client/components/app/feed/Post.jsx
@@ -9,7 +9,7 @@ export default function Post({ post }) {
         <div className={styles.container}>
             <div className={styles.topper}>
                 <div className={styles.profile}>
-                    <Avatar width={70} height={70} src={post.creator.profile} />
+                    <Avatar width={70} height={70} src={post.creator.profile} name={post.creator.name} />
                     <div style={{ marginLeft: "10px" }}>
                         <Text fontSize="xl">{post.creator.name}</Text>
                         <Text style={{ color: "#CBD5E0" }} fontSize="lg">{post.creator.email}</Text>
--- client/components/app/users/User.jsx
+++ client/components/app/users/User.jsx
@@ -3,7 +3,7 @@ import { Text, Avatar } from '@chakra-ui/react';
 export default function User({ user }) {
     return (
         <div style={{ display: "flex", justifyContent: "space-between", marginTop: "10px" }}>
-            <Avatar src={user.profile} />
+            <Avatar src={user.profile} name={user.name} />
             <div style={{ marginLeft: "10px" }}>
                 <Text fontSize="lg">{user.name}</Text>
                 <Text style={{ color: "#CBD5E0" }} fontSize="md">{user.email}</Text>
