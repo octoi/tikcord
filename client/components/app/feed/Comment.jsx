0a7de6fd10f570811e3687de0b583c4eab39b94d
fadhilsaheer <fadhilsaheer@gmail.com>
2021-05-04 22:11:08 +0530

    create comment

--- client/components/app/feed/Comment.jsx
+++ client/components/app/feed/Comment.jsx
@@ -1,4 +1,6 @@
+import COMMENT_POST_QUERY from '../../../utils/graphql/commentPostQuery';
 import { useState } from 'react';
+import { useMutation } from '@apollo/client';
 import {
     Button,
     Input,
@@ -15,8 +17,26 @@ import {
 
 
 
-export default function Comment({ isOpen, onClose, postComments }) {
+export default function Comment({ isOpen, onClose, post }) {
     const [comment, setComment] = useState("");
+
+    const [CommentPost] = useMutation(COMMENT_POST_QUERY, {
+        variables: { content: comment, post: post.id },
+        update: () => {
+            alert("created comment")
+        },
+        onError: (err) => {
+            console.log(err.message);
+        }
+    })
+
+    const commentPost = () => {
+        if (comment.trim().length === 0) {
+            alert("Can you please comment ...");
+            return;
+        }
+        CommentPost();
+    }
 
     return (
         <Modal isOpen={isOpen} onClose={onClose} isCentered>
@@ -30,7 +50,7 @@ export default function Comment({ isOpen, onClose, postComments }) {
                         value={comment}
                         onChange={(event) => setComment(event.target.value)}
                     />
-                    <Button width="100%" mt={5}>Comment</Button>
+                    <Button onClick={commentPost} width="100%" mt={5}>Comment</Button>
                 </ModalBody>
             </ModalContent>
         </Modal>
