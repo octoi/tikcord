75363101c678549326de4a672705fbc777129bc2
fadhilsaheer <fadhilsaheer@gmail.com>
2021-05-05 13:25:57 +0530

    displayed skeleton

--- client/components/app/feed/AllPosts.jsx
+++ client/components/app/feed/AllPosts.jsx
@@ -5,8 +5,13 @@ import { Spinner } from '@chakra-ui/react';
 export default function AllPosts({ posts, loading }) {
     return (
         <div>
-            {loading || !loading && (
-                <Skeleton />
+            {loading && (
+                <div>
+                    <Skeleton />
+                    <Skeleton />
+                    <Skeleton />
+                    <Skeleton />
+                </div>
             )}
             {posts && posts.map(post => (
                 <Post key={post.id} post={post} />
