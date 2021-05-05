b3fce39f42310fcb055f89d1d35dd0339780ad52
fadhilsaheer <fadhilsaheer@gmail.com>
2021-05-05 13:27:39 +0530

    removed update user func

--- server/postgres/helpers/userHelper.js
+++ server/postgres/helpers/userHelper.js
@@ -6,31 +6,19 @@ module.exports = {
     registerUser: (name, email, password, bio, profile) => {
         return new Promise((resolve, reject) => {
             checkUserExists(email).then(data => {
-                if(data.rows.length == 0){
+                if (data.rows.length == 0) {
 
                     pool.query(`INSERT INTO ${userTable} (name, email, password, bio, profile) VALUES ($1, $2, $3, $4, $5)`, [name, email, password, bio, profile]).then(data => {
                         resolve({ name, email, password, bio, profile });
                     }).catch(err => {
                         console.log(err.message)
-                        reject(); 
+                        reject();
                     })
 
-                }else{
+                } else {
                     reject();
                 }
             })
         });
     },
-
-    updateUserData: (name, password, email, bio, profile) => {
-        return new Promise((resolve, reject) =>  {
-            pool.query(`UPDATE ${userTable} SET name = $1, password = $2, bio = $3, profile = $4 WHERE email = $5`, [name, password, bio, profile, email]).then(data => {
-                console.log(data)
-                resolve({ name, email, password, bio, profile })
-            }).catch(err => {
-                console.log(err.message)
-                reject(); 
-            });
-        });
-    }
 }
