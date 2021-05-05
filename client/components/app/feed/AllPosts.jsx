78cc9039706f4e782021a5f23cbfd7cd2073b536
fadhilsaheer <fadhilsaheer@gmail.com>
2021-05-05 13:25:14 +0530

    skeleton

--- client/components/app/feed/AllPosts.jsx
+++ client/components/app/feed/AllPosts.jsx
@@ -1,8 +1,13 @@
 import Post from './Post';
+import Skeleton from './Skeleton';
+import { Spinner } from '@chakra-ui/react';
 
 export default function AllPosts({ posts, loading }) {
     return (
         <div>
+            {loading || !loading && (
+                <Skeleton />
+            )}
             {posts && posts.map(post => (
                 <Post key={post.id} post={post} />
             ))}
---
+++ client/components/app/feed/Skeleton.jsx
@@ -0,0 +1,7 @@
+import { Skeleton } from '@chakra-ui/react';
+
+export default function SkeletonBody() {
+    return (
+        <Skeleton mt={10} height={400} />
+    )
+}
