29e02275e0801304291b7a43df33351ec1de0ed4
fadhilsaheer <fadhilsaheer@gmail.com>
2021-05-05 06:49:40 +0530

    basic check

--- client/pages/account/[email].js
+++ client/pages/account/[email].js
@@ -1,14 +1,17 @@
+import useAuthContext from '../../context/contextHook';
 import { useRouter } from 'next/router';
 
 export default function Profile() {
     const router = useRouter();
 
-
+    const { user } = useAuthContext();
     const { email } = router.query;
+
+    if (!user) router.push("/account/login")
+    if (!email) router.push("/app")
 
     return (
         <section>
-            {email}
         </section>
     );
 
