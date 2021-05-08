f6980c09005a1479b54f3644ffb5428e44d83ab1
fadhilsaheer <fadhilsaheer@gmail.com>
2021-05-08 12:23:56 +0530

    added simple socket server

--- server/server.js
+++ server/server.js
@@ -1,12 +1,19 @@
 require("dotenv").config();
 
-const redis = require("./redis");
+const socketIo = require("socket.io");
 const { ApolloServer } = require("apollo-server");
 
+const redis = require("./redis");
 const typeDefs = require("./graphql/typeDefs");
 const resolvers = require("./graphql/resolvers");
 
 const server = new ApolloServer({ typeDefs, resolvers, context: ({ req }) => ({ req }) }); // graphql server
 
+const io = socketIo(server);
+
+io.on("connection", () => {
+    console.log("user connected")
+})
+
 const port = process.env.PORT || 5000;
-server.listen(port).then(({ url })=> console.log(`[INFO] Server started at ${url}`))
+server.listen(port).then(({ url }) => console.log(`[INFO] Server started at ${url}`))
