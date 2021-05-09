e80d33be799ab9d874d7fd62aa1fee764cff8964
fadhilsaheer <fadhilsaheer@gmail.com>
2021-05-09 22:23:48 +0530

    made connection

--- client/context/SocketContext.js
+++ client/context/SocketContext.js
@@ -1,15 +1,24 @@
 import io from 'socket.io-client';
+import useUser from './contextHook';
 import { createContext, useState, useEffect } from 'react';
 
 export const SocketStateContext = createContext();
 
 export function SocketContext({ children, serverUrl }) {
+    const { user } = useUser();
     const [socket, setSocket] = useState();
 
     useEffect(() => {
         let sock = io(serverUrl, { transports: ["websocket"] });
         setSocket(sock);
-    }, [])
+    }, [serverUrl])
+
+    useEffect(() => {
+        if (!socket || !user) return;
+
+        socket.emit("make-connection", user)
+
+    }, [socket])
 
     return (
         <SocketStateContext.Provider value={{ socket }}>
