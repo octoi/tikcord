8b0090ca5885f460735b65bd4dda7efeb3d42418
fadhilsaheer <fadhilsaheer@gmail.com>
2021-05-09 21:39:16 +0530

    implemented settings popup

--- client/components/app/Sidebar.jsx
+++ client/components/app/Sidebar.jsx
@@ -1,6 +1,7 @@
 import useAuthContext from '../../context/contextHook';
 import IconButton from './sidebar/IconButtton';
 import NewPost from './NewPost';
+import Settings from './Settings';
 import styles from '../../styles/App.module.css';
 import { HomeIcon, UserIcon, CogIcon, PlusCircleIcon } from '@heroicons/react/solid'
 import { useDisclosure } from '@chakra-ui/react';
@@ -14,9 +15,10 @@ export default function Sidebar() {
         <div className={styles.sidebar}>
             <IconButton Icon={HomeIcon} text="Home" link="/app" />
             <IconButton Icon={UserIcon} text="Profile" link={`/account/${user.email}`} mt />
-            <IconButton Icon={CogIcon} text="Settings" mt />
+            <IconButton Icon={CogIcon} text="Settings" onClick={settingsPopup.onOpen} mt />
             <IconButton Icon={PlusCircleIcon} text="Create" mt solid onClick={newPostPopup.onOpen} />
             <NewPost isOpen={newPostPopup.isOpen} onClose={newPostPopup.onClose} />
+            <Settings isOpen={settingsPopup.isOpen} onClose={settingsPopup.onClose} />
         </div>
     );
 }
