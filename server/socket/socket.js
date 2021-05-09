6e0780e5ae7a101f7d997a7bdd0db6358173a4e5
fadhilsaheer <fadhilsaheer@gmail.com>
2021-05-09 22:28:37 +0530

    simplified connection

--- server/socket/socket.js
+++ server/socket/socket.js
@@ -1,13 +1,9 @@
 module.exports = (socket) => {
 
-    socket.on("make-connection", (userData, callback) => {
-        socket.join("post-connection");
-        // add user 
-        callback(socket.id)
-    });
+    socket.on("make-connection", () => socket.join("post-connection"));
 
     socket.on("disconnect", () => {
-        // disconnect user         
+        // disconnect user
     });
 
 }
