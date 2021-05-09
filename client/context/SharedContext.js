1765c68bf85e1dd34c365cb8717c62b117037017
fadhilsaheer <fadhilsaheer@gmail.com>
2021-05-09 23:10:10 +0530

    refactored and added new state

--- client/context/SharedContext.js
+++ client/context/SharedContext.js
@@ -4,9 +4,17 @@ export const SharedStateContext = createContext();
 
 export function SharedContext({ children }) {
     const [fetchPostData, setFetchPostData] = useState(true);
+    const [onlineUsers, setOnlineUser] = useState();
+
+    const props = {
+        fetchPostData,
+        setFetchPostData,
+        onlineUsers,
+        setOnlineUser
+    }
 
     return (
-        <SharedStateContext.Provider value={{ fetchPostData, setFetchPostData }}>
+        <SharedStateContext.Provider value={props}>
             {children}
         </SharedStateContext.Provider>
     );
