349ae6e9b6532cc96cf7c84d664c6d4597bc6d68
fadhilsaheer <fadhilsaheer@gmail.com>
2021-05-09 21:55:19 +0530

    sample radios

--- client/components/app/Settings.jsx
+++ client/components/app/Settings.jsx
@@ -1,15 +1,24 @@
+import cookie from 'js-cookie';
+import { useState, useEffect } from 'react';
 import {
-    Button,
     Modal,
     ModalOverlay,
     ModalHeader,
     ModalCloseButton,
-    ModalFooter,
     ModalBody,
     ModalContent,
+    Radio,
+    Stack,
+    RadioGroup
 } from '@chakra-ui/react';
 
 export default function NewPost({ isOpen, onClose }) {
+    const [reloadOption, setReloadOption] = useState("default");
+
+    // useEffect(() => {
+    //     let setting = JSON.parse(cookie.get("setting"));
+    //     setReloadOption(setting.option);
+    // }, []);
 
     return (
         <Modal isOpen={isOpen} onClose={onClose} isCentered>
@@ -18,18 +27,14 @@ export default function NewPost({ isOpen, onClose }) {
                 <ModalHeader>Settings</ModalHeader>
                 <ModalCloseButton />
                 <ModalBody>
+                    <RadioGroup defaultValue={reloadOption} onChange={(val) => setReloadOption(val)}>
+                        <Stack spacing={5} direction="column">
+                            <Radio colorScheme="twitter" value={"default"}>Ask me, What to do ??</Radio>
+                            <Radio colorScheme="twitter" value={"reload"}>Reload every time without asking</Radio>
+                            <Radio colorScheme="twitter" value={"no_reload"}>Never ask me or reload</Radio>
+                        </Stack>
+                    </RadioGroup>
                 </ModalBody>
-
-                <ModalFooter>
-                    <Button colorScheme="twitter">Create</Button>
-                    <Button
-                        ml={3}
-                        onClick={() => {
-                            onClose();
-                        }}
-                        variant="ghost"
-                    >Cancel</Button>
-                </ModalFooter>
             </ModalContent>
         </Modal>
     );
