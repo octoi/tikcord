2c0445017019d45a16fb12583a99fb2605742424
fadhilsaheer <fadhilsaheer@gmail.com>
2021-05-09 22:59:52 +0530

    helper functions

--- server/redis/userSocketHelper.js
+++ server/redis/userSocketHelper.js
@@ -1,4 +1,23 @@
 const client = require("./index");
 
 module.exports = {
+    addUser: (userData, socketId) => {
+        return new Promise((resolve, reject) => {
+            client.HSET("online-users", socketId, userData, (err, data) => {
+                if (err) reject(err);
+                console.log(data)
+                resolve(err);
+            })
+        });
+    },
+
+    getOnlineUsers: () => {
+        return new Promise((resolve, reject) => {
+            client.HGETALL("online-users", (err, data) => {
+                if (err) reject(err);
+                resolve(data)
+            })
+        });
+    }
+
 }
