d9a2ad5736d320af287bbc548fe4b060283c82bf
fadhilsaheer <fadhilsaheer@gmail.com>
2021-05-09 22:59:56 +0530

    socket connection

--- server/socket/socket.js
+++ server/socket/socket.js
@@ -1,6 +1,16 @@
+
+const userSocketHelper = require("../redis/userSocketHelper");
+
 module.exports = (socket, io) => {
 
-    socket.on("make-connection", () => socket.join("post-connection"));
+    socket.on("make-connection", (userData, callback) => {
+        userSocketHelper.addUser(JSON.stringify(userData), socket.id).then(() => {
+            socket.join("online");
+            userSocketHelper.getOnlineUsers().then(data => {
+                callback({ id: socket.id, onlineUser: data })
+            })
+        })
+    });
 
     socket.on("disconnect", () => {
         // disconnect user
