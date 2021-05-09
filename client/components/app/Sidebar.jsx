eac9bc747a78ad47da1a314db2ddb57b98ff8231
fadhilsaheer <fadhilsaheer@gmail.com>
2021-05-09 21:36:10 +0530

    created basic settings btn

--- client/components/app/Sidebar.jsx
+++ client/components/app/Sidebar.jsx
@@ -7,12 +7,14 @@ import { useDisclosure } from '@chakra-ui/react';
 
 export default function Sidebar() {
     const newPostPopup = useDisclosure();
+    const settingsPopup = useDisclosure();
     const { user } = useAuthContext();
 
     return (
         <div className={styles.sidebar}>
             <IconButton Icon={HomeIcon} text="Home" link="/app" />
             <IconButton Icon={UserIcon} text="Profile" link={`/account/${user.email}`} mt />
+            <IconButton Icon={CogIcon} text="Settings" mt />
             <IconButton Icon={PlusCircleIcon} text="Create" mt solid onClick={newPostPopup.onOpen} />
             <NewPost isOpen={newPostPopup.isOpen} onClose={newPostPopup.onClose} />
         </div>
