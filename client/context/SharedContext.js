203bd3a8403273fcdae1e87152c82df57dd7b36e
fadhilsaheer <fadhilsaheer@gmail.com>
2021-05-10 11:13:09 +0530

    removed emit update

--- client/context/SharedContext.js
+++ client/context/SharedContext.js
@@ -5,15 +5,12 @@ export const SharedStateContext = createContext();
 export function SharedContext({ children }) {
     const [fetchPostData, setFetchPostData] = useState(true);
     const [onlineUsers, setOnlineUsers] = useState([]);
-    const [emitUpdate, setEmitUpdate] = useState(false);
 
     const props = {
         fetchPostData,
         setFetchPostData,
         onlineUsers,
         setOnlineUsers,
-        emitUpdate,
-        setEmitUpdate
     }
 
     return (
