6abf92bcd463b6b03b3620dc82e6757c4119c885
fadhilsaheer <fadhilsaheer@gmail.com>
2021-05-11 07:35:10 +0530

    getting settings

--- client/context/SharedContext.js
+++ client/context/SharedContext.js
@@ -1,16 +1,25 @@
-import { createContext, useState } from 'react';
+import cookie from 'js-cookie';
+import { createContext, useState, useEffect } from 'react';
 
 export const SharedStateContext = createContext();
 
 export function SharedContext({ children }) {
     const [fetchPostData, setFetchPostData] = useState(true);
     const [onlineUsers, setOnlineUsers] = useState([]);
+    const [setting, setSetting] = useState("default");
+
+    useEffect(() => {
+        let reloadSetting = cookie.get("setting")
+        if (reloadSetting) setSetting(reloadSetting)
+    }, []);
 
     const props = {
         fetchPostData,
         setFetchPostData,
         onlineUsers,
         setOnlineUsers,
+        setting,
+        setSetting
     }
 
     return (
